b601a6cef95d3888f9a1f27e5fb12860bccd7143
Oleksandr Shynkarov <81474894+shinkaryov@users.noreply.github.com>
2022-11-16 19:27:09 +0200

    Delete readress_to_spheres_is_working_om.py

--- readress_to_spheres_is_working_om.py
+++
@@ -1,9 +0,0 @@
-from behave import *
-
-@when('press button "{sphere}"')
-def step_impl(context, sphere):
-    pass
-
-@then('site readress  to "https://www.epam.com/services/{sphere}"')
-def step_impl(context, sphere):
-    pass
