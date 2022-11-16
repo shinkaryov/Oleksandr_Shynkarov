a1b36b34e7017ea864dd91ffe35c9fd23230cc85
Oleksandr Shynkarov <81474894+shinkaryov@users.noreply.github.com>
2022-11-16 19:26:39 +0200

    Delete entering_any_adress_in_domain.py

--- entering_any_adress_in_domain.py
+++
@@ -1,9 +0,0 @@
-from behave import *
-
-@when('go to site "https://www.epam.com/{domain}"')
-def step_impl(context, domain):
-    pass
-
-@then('site will open error 404 in EPAM domain')
-def step_impl(context):
-    pass
