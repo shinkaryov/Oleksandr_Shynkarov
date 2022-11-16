2d8074257a676b90d51cf538abcd92bd38d51db4
Oleksandr Shynkarov <81474894+shinkaryov@users.noreply.github.com>
2022-11-16 19:26:19 +0200

    Delete Inspecting_button_find_your_dream_job.py

--- Inspecting_button_find_your_dream_job.py
+++
@@ -1,9 +0,0 @@
-from behave import *
-
-@when('we push button named "Find your dream job"')
-def step_impl(context):
-    pass
-
-@then('page readresses to "https://www.epam.com/careers"')
-def step_impl(context):
-    pass
