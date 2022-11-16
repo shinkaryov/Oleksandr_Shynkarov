79ea2a8038ec627359aedf3b980e6413c3cbd0d3
Oleksandr Shynkarov <81474894+shinkaryov@users.noreply.github.com>
2022-11-16 19:27:55 +0200

    Add files via upload

---
+++ Steps/reloading_of_website.py
@@ -0,0 +1,13 @@
+from behave import *
+
+@given('"https://www.epam.com/" is opened')
+def step_impl(context):
+    pass
+
+@when('push "Ctrl+R" on keybord')
+def step_impl(context):
+    pass
+
+@then('site will reload')
+def step_impl(context):
+    pass
---
+++ Steps/scaling_change.py
@@ -0,0 +1,9 @@
+from behave import *
+
+@when('reduce browser scale')
+def step_impl(context):
+    pass
+
+@then('site will reduce its scale')
+def step_impl(context):
+    pass
