6fe3aadfebb178736e939ce5c2e6802f31d277c2
Oleksandr Shynkarov <81474894+shinkaryov@users.noreply.github.com>
2022-11-16 19:26:29 +0200

    Delete country_check.py

--- country_check.py
+++
@@ -1,9 +0,0 @@
-from behave import *
-
-@when('place cursor on "{country}"')
-def step_impl(context, country):
-    pass
-
-@then('image of {"country} will change"')
-def step_impl(context, country):
-    pass
